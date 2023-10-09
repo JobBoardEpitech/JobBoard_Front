2a42cc3449ffa02995d62224af4e30f6922bcc65
Antoine <gourgue.antoine@esh64.fr>
2023-10-09 18:53:35 +0200

    feat : setup project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <h1>aaaa</h1>
+  </header>
+  <body class="bg-gray-800">
+
+  </body>
+  <main>
+
+  </main>
+  <Footer/>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        screens: {
+            xs: '375px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+            '3xl': '1920px',
+            '4xl': '2560px'
+        },
+        extend: {},
+    },
+    plugins: [],
+}

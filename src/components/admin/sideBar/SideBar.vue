c9f01cb4fa23508e39ae831288a475fb1b21ace6
Antoine <gourgue.antoine@esh64.fr>
2023-10-14 22:37:45 +0200

    feat : sideBar dashboard and client manager

--- src/components/admin/sideBar/SideBar.vue
+++ src/components/admin/sideBar/SideBar.vue
@@ -2,8 +2,7 @@
   <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
   <link href='https://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet'>
 
-  <div class="min-h-screen flex flex-row bg-gray-100">
-    <div class="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
+    <div class="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden h-screen">
       <div class="flex items-center justify-center h-20 shadow-md">
         <a href="/" class="font-fredoka font-semibold flex items-center text-[#4341C0] text-2xl">
           JOBBOARD
@@ -11,22 +10,29 @@
       </div>
       <ul class="flex flex-col py-4">
         <li>
-          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <router-link
+              :to="{name: 'client-manager'}"
+              role="button"
+              class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
             <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-user"></i></span>
             <span class="text-sm font-medium">Users</span>
-          </a>
+          </router-link>
         </li>
         <li>
-          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <router-link
+              :to="{name: 'client-manager'}"
+              role="button" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
             <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-notepad"></i></span>
             <span class="text-sm font-medium">Advertisements</span>
-          </a>
+          </router-link>
         </li>
         <li>
-          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <router-link
+              :to="{name: 'companies-manager'}"
+              role="button" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
             <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-building-house"></i></span>
             <span class="text-sm font-medium">Companies</span>
-          </a>
+          </router-link>
         </li>
         <li>
           <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
@@ -36,9 +42,7 @@
         </li>
       </ul>
     </div>
-  </div>
 </template>
-
 
 <script setup lang="ts">
 </script>
--- src/views/admin/ClientManagerView.vue
+++ src/views/admin/ClientManagerView.vue
@@ -1,7 +1,14 @@
 <template>
+  <div class="h-screen flex bg-gray-200">
     <SideBar/>
+    <UserClientCard
+        :users="users"
+    />
+  </div>
 </template>
 <script setup lang="ts">
 import SideBar from "@/components/admin/sideBar/SideBar.vue";
 import UserClientCard from "@/components/admin/cards/UserClientCard.vue";
+import {users} from "@/services/user";
+
 </script>

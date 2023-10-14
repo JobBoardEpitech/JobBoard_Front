8e89ba436befd4c1530371bb7c1b8294c66cf9c9
Antoine <gourgue.antoine@esh64.fr>
2023-10-14 23:21:06 +0200

    feat : logout sideBar

--- src/components/admin/sideBar/SideBar.vue
+++ src/components/admin/sideBar/SideBar.vue
@@ -35,10 +35,12 @@
           </router-link>
         </li>
         <li>
-          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <router-link
+              :to="{name: 'login'}"
+              role="button" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
             <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-log-out"></i></span>
             <span class="text-sm font-medium">Logout</span>
-          </a>
+          </router-link>
         </li>
       </ul>
     </div>

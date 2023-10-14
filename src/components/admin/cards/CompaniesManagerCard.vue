cabf9324e30ac1efe5b6f38740852c88bf9e1075
Antoine <gourgue.antoine@esh64.fr>
2023-10-14 23:18:44 +0200

    feat : advertisements manager

--- src/components/admin/cards/CompaniesManagerCard.vue
+++ src/components/admin/cards/CompaniesManagerCard.vue
@@ -1,25 +1,65 @@
 <template>
-  <div id="container" class="bg-[#0d121c] text-center rounded-lg mx-auto my-20 h-[calc(100vh-40px)] w-[calc(90vw-40px)] flex items-center justify-center">
-    <div class="overflow-hidden rounded-lg shadow-md m-5 ">
-      <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
-        <thead class="bg-gray-50">
-        <tr>
-          <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
-          <th scope="col" class="px-6 py-4 font-medium text-gray-900">State</th>
-          <th scope="col" class="px-6 py-4 font-medium text-gray-900">Role</th>
-          <th scope="col" class="px-6 py-4 font-medium text-gray-900">Team</th>
-          <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
-        </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-        <!--  LIST      -->
-        </tbody>
-      </table>
-      <pagination-manager/>
+  <div class="px-4 py-8 sm:px-8 w-full">
+    <div class="flex flex-col overflow-y-hidden rounded-lg border h-full">
+      <div class="overflow-x-auto flex-1">
+        <table class="w-full">
+          <thead class="sticky top-0">
+          <tr class="bg-blue-600 text-left text-xs font-semibold uppercase tracking-widest text-white">
+            <th class="px-5 py-3">ID</th>
+            <th class="px-5 py-3">Name</th>
+            <th class="px-5 py-3">Description</th>
+            <th class="px-5 py-3">Actions</th>
+          </tr>
+          </thead>
+          <tbody class="text-gray-500">
+          <tr
+              v-for="company in props.companies"
+              :key="company.id"
+          >
+            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+              <p class="whitespace-no-wrap">
+                {{company.id}}
+              </p>
+            </td>
+            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+              <div class="flex items-center">
+                <p class="whitespace-no-wrap">
+                  {{ company.name }}
+                </p>
+              </div>
+            </td>
+            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+              <p class="whitespace-no-wrap truncate">
+                {{ company.description }}
+              </p>
+            </td>
+            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+              <button>
+                D
+              </button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="flex flex-col items-center border-t px-5 pt-5 sm:flex-row sm:justify-between">
+        <span class="text-xs text-gray-600 sm:text-sm"> Showing 1 to 5 of 12 Entries </span>
+        <div class="mt-2 inline-flex sm:mt-0">
+          <button class="mr-2 h-12 w-12 rounded-full border text-sm font-semibold text-gray-600 transition duration-150 hover:bg-gray-100">Prev</button>
+          <button class="h-12 w-12 rounded-full border text-sm font-semibold text-gray-600 transition duration-150 hover:bg-gray-100">Next</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import PaginationManager from "@/components/admin/pagination/PaginationManager.vue";
+import {PropType} from "vue";
+import type {Company} from "@/services/companies";
+
+const props = defineProps({
+  companies: {
+    type: Array as PropType<Company[]>
+  }
+})
 </script>
--- src/views/admin/CompaniesManagerView.vue
+++ src/views/admin/CompaniesManagerView.vue
@@ -1,20 +1,15 @@
 <template>
-  <head>
-    <meta charset="UTF-8">
-    <title>JobBoard Admin</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
-  </head>
-  <body class="bg-[#121b2d] text-[#f3f5f9] font-inter flex items-center h-screen p-4 gap-4">
+  <div class="h-screen flex bg-gray-200">
+    <SideBar/>
+    <CompaniesManagerCard
+        :companies="companies"
+    />
+  </div>
 
-  <SideBar/>
-
-  <companies-manager-card/>
-  </body>
-  <Footer/>
 </template>
 <script setup lang="ts">
 import SideBar from "@/components/admin/sideBar/SideBar.vue";
+import AdvertisementsManagerCard from "@/components/admin/cards/AdvertisementsManagerCard.vue";
+import {companies} from "@/services/companies";
 import CompaniesManagerCard from "@/components/admin/cards/CompaniesManagerCard.vue";
 </script>
